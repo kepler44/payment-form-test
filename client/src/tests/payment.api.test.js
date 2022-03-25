44f5a452b13514a113104ad3aa2d336621c5580e
Duke <no email>
2022-03-25 19:34:16 +0000

    added tests

--- client/src/tests/payment.api.test.js
+++ client/src/tests/payment.api.test.js
@@ -80,29 +80,3 @@ describe("Test payment form", () => {
 	});
 });
 
-/* 
-//GET all users      
-describe("GET all user details", () => {
-  
-  try{
-      beforeEach(function () {
-        console.log("GET all users details ")
-    });
-          
-      afterEach(function () {
-        console.log("All users' details are retrieved")
-    });
-
-      test("GET user output", async done =>{
-        await request.request.get(`api/users`) //get() of supertest
-                                //.set('Authorization', `Token ${request.token}`) 
-                                .expect(200).then((response) =>{
-                                console.log("GET RESPONSE : ", response.body);
-                                done();
-                    })
-      })
-    }
-  catch(err){
-    console.log("Exception : ", err)
-    }
-}); */
